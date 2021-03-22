562531ecd76702d691a15b6b244fec08d5351aac
Jakub Syska <jaksyska@gmail.com>
2021-03-22 11:11:11 +0100

    quite large initial commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,6 +15,7 @@ public class Main extends Application {
         primaryStage.setTitle("Scheduling Alghoritms");
         primaryStage.setScene(new Scene(root, 750, 513));
         primaryStage.show();
+        primaryStage.getIcons().add(new Image("icon.png"));
     }
 
 
